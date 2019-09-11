6001e5b0d6d94ceb794baf9c01618946885dfc58
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 12:36:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,13 @@
-speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end 
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(speaker)
-attendees.map do |speaker|
- "Hello, my name is #{speaker}."
+
+def badge_maker (name)
+return"Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+attendees.map do |attendee|
+ "Hello, my name is #{attendee}."
  end
 end
