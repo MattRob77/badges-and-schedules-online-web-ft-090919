368e202adad38ece9af7367a37e244c22ae8efa3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 12:33:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(speakers)
-attendees.map do ||
- "Hello, my name is #{attendee}."
+attendees.map do |speakers|
+ "Hello, my name is #{speakers}."
  end
 end
